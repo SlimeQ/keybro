8c0a541fce74abbeaf398d9b384c612f94b41011
SlimeQ <quincy.c.campbell@wmich.edu>
2015-04-02 10:08:40 -0400

    fixed error with key model

--- app/controllers/keys_controller.rb
+++ app/controllers/keys_controller.rb
@@ -1,15 +1,12 @@
 class KeysController < ApplicationController
   def new
   end
+  def index
+  end
   def create
-    begin
-      @key = Key.new(key_params)
-    rescue ArgumentError
-      # deal with the error here
-      puts ArgumentError
-    end
-    #@key = Key.build_from_scale(key_params)
-    # @key.save
+    @key = Key.new(key_params)
+    # @key = Key.build_from_scale(key_params)
+    @key.save
     redirect_to @key
   end
   def show
--- app/models/key.rb
+++ app/models/key.rb
@@ -1,43 +1,76 @@
 class Key < ActiveRecord::Base
-  def after_initialize(params)
-    @scale = params[:scale]
-    @root = params[:root]
-    # @fretboard = data
-    # @interval = "I"
-  end
-  def shift_mode(numeral)
-    # @fretboard.each { |e|
-    #   e.each { |f|
-    #     print f
-    #   }
-    #   print "\n"
-    # }
-    retval = @fretboard.each.map {|y|
-      y = y.each.map {|x|
-        if x != 0
-          x -= numeral-1
-          if x <= 0
-            x += 7
-          else
-            x
-          end
+  # def after_initialize(params)
+  #   @scale = params[:scale]
+  #   @root = params[:root]
+  #   # @fretboard = data
+  #   # @interval = "I"
+  # end
+  def fretboard
+    # steps = scale.split("").map {|x| x.to_i}
+    starting_fret = root.to_i
+
+    # fill in master string
+    i = 0
+    interval = 1
+    master = []
+    master.push(interval)
+    interval += 1
+    scale.split("").each {|step|
+      x = step.to_i
+      while x > 0 do
+        if x == 1
+          master.push(interval)
+          interval += 1
         else
-          x
+          master.push(0)
         end
-      }
+        x -= 1
+      end
     }
-    # retval.each { |e|
-    #   e.each { |f|
-    #     print f
-    #   }
-    #   print "\n"
-    # }
-    # puts retval
-    return Fretboard.new(retval)
+
+    # remove last note and duplicate sufficient times
+    master.pop
+    str = master
+    master *= 6
+
+    # add stuff to beginning
+    master = str[-root.to_i..-1] + master
+    return [master[26, 24], master[21, 24], master[17,24], master[12,24], master[7,24], master[0,24]]
   end
+  # def shift_mode(numeral)
+  #   # @fretboard.each { |e|
+  #   #   e.each { |f|
+  #   #     print f
+  #   #   }
+  #   #   print "\n"
+  #   # }
+  #   retval = @fretboard.each.map {|y|
+  #     y = y.each.map {|x|
+  #       if x != 0
+  #         x -= numeral-1
+  #         if x <= 0
+  #           x += 7
+  #         else
+  #           x
+  #         end
+  #       else
+  #         x
+  #       end
+  #     }
+  #   }
+  #   # retval.each { |e|
+  #   #   e.each { |f|
+  #   #     print f
+  #   #   }
+  #   #   print "\n"
+  #   # }
+  #   # puts retval
+  #   return Fretboard.new(retval)
+  # end
   def print_chord(chord)
     y = 1
-    @fretboard.each do |string|
+    retval = ""
+    fretboard.each do |string|
       output = ""
       output = "e" if y == 1
       output = "B" if y == 2
@@ -71,78 +104,78 @@ class Key < ActiveRecord::Base
         end
         x += 1
       end
-      puts output
+      retval += "#{output}<br>"
       y += 1
     end
-    puts "              *       *       *       *          * *          *       *       *       *"
+    "#{retval}              *       *       *       *          * *          *       *       *       *"
   end
-  def pretty_print
-    y = 1
-    @fretboard.each do |string|
-      output = ""
-      output = "e|" if y == 1
-      output = "B|" if y == 2
-      output = "G|" if y == 3
-      output = "D|" if y == 4
-      output = "A|" if y == 5
-      output = "D|" if y == 6
+  # def pretty_print
+  #   y = 1
+  #   @fretboard.each do |string|
+  #     output = ""
+  #     output = "e|" if y == 1
+  #     output = "B|" if y == 2
+  #     output = "G|" if y == 3
+  #     output = "D|" if y == 4
+  #     output = "A|" if y == 5
+  #     output = "D|" if y == 6
 
-      x = 0
-      string.each do |fret|
-        if x == 0
-          if fret == 0
-            output += " ||"
-          else
-            output += "#{fret}||"
-          end
-        else
-          if fret == 0
-            output += "---|"
-          else
-            output += " #{fret} |"
-          end
-        end
-        x += 1
-      end
-      puts output
-      y += 1
-    end
-    puts "              *       *       *       *          * *          *       *       *       *"
-  end
-  def self.build_from_scale(params)
+  #     x = 0
+  #     string.each do |fret|
+  #       if x == 0
+  #         if fret == 0
+  #           output += " ||"
+  #         else
+  #           output += "#{fret}||"
+  #         end
+  #       else
+  #         if fret == 0
+  #           output += "---|"
+  #         else
+  #           output += " #{fret} |"
+  #         end
+  #       end
+  #       x += 1
+  #     end
+  #     puts output
+  #     y += 1
+  #   end
+  #   puts "              *       *       *       *          * *          *       *       *       *"
+  # end
+  # def self.build_from_scale(params)
 
-    scale = params[:scale].split("").map {|x| x.to_i}
-    starting_fret = params[:root].to_i
+  #   scale = params[:scale].split("").map {|x| x.to_i}
+  #   starting_fret = params[:root].to_i
 
-    # fill in master string
-    i = 0
-    interval = 1
-    master = []
-    master.push(interval)
-    interval += 1
-    scale.each {|step|
-      x = step
-      while x > 0 do
-        if x == 1
-          master.push(interval)
-          interval += 1
-        else
-          master.push(0)
-        end
-        x -= 1
-      end
-    }
+  #   # fill in master string
+  #   i = 0
+  #   interval = 1
+  #   master = []
+  #   master.push(interval)
+  #   interval += 1
+  #   scale.each {|step|
+  #     x = step
+  #     while x > 0 do
+  #       if x == 1
+  #         master.push(interval)
+  #         interval += 1
+  #       else
+  #         master.push(0)
+  #       end
+  #       x -= 1
+  #     end
+  #   }
 
-    # remove last note and duplicate sufficient times
-    master.pop
-    str = master
-    master *= 6
+  #   # remove last note and duplicate sufficient times
+  #   master.pop
+  #   str = master
+  #   master *= 6
 
-    # add stuff to beginning
-    master = str[-starting_fret..-1] + master
-    master.each {|x| print x}
-    print "\n"
+  #   # add stuff to beginning
+  #   master = str[-starting_fret..-1] + master
+  #   master.each {|x| print x}
+  #   print "\n"
 
-    return Key.new([master[26, 24], master[21, 24], master[17,24], master[12,24], master[7,24], master[0,24]])
-  end
+  #   return Key.new([master[26, 24], master[21, 24], master[17,24], master[12,24], master[7,24], master[0,24]])
+  # end
 end
